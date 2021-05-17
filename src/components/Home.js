b39ed9ef259678f002f627b912e81cdfdbdc791f
021120 <yongtatt981213@hotmail.com>
2021-05-17 16:50:15 +0800

    Left side

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,8 @@
 import React from "react";
 import styled from "styled-components";
+import Leftside from "./Leftside";
+import Main from "./Main";
+import Rightside from "./Rightside";
 
 const Home = () => {
   return (
@@ -13,6 +16,11 @@ const Home = () => {
           moving.
         </p>
       </Section>
+      <Layout>
+        <Leftside />
+        <Main />
+        <Rightside />
+      </Layout>
     </Container>
   );
 };
@@ -43,6 +51,21 @@ const Section = styled.section`
     font-weight: 600;
   }
   @media (max-width: 768px) {
+    flex-direction: column;
+    padding: 0 5px;
+  }
+`;
+
+const Layout = styled.div`
+  display: grid;
+  grid-template-areas: "leftside main rightside";
+  grid-template-columns: minmax(0, 5fr), minmax(0, 12fr), minmax(300px, 7fr);
+  column-gap: 25px;
+  row-gap: 25px;
+
+  margin: 25px 0;
+  @media (max-width: 768px) {
+    display: flex;
     flex-direction: column;
     padding: 0 5px;
   }
---
+++ src/components/Rightside.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Rightside = () => {
+  return <Container>Rightside</Container>;
+};
+
+const Container = styled.div`
+  grid-area: rightside;
+`;
+
+export default Rightside;
